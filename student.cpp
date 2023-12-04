283e0d0cc51cccbc501d0fc1eb41c72a12c4431c
U-BSD\452215 <452215@SHS-C1W2NW3.beaverton.k12.or.us>
2023-12-04 10:27:06 -0800

    working

---
+++ student.cpp
@@ -0,0 +1,15 @@
+#include "student.h"
+
+Student::Student(Node*){
+  value = 0;
+  next = NULL
+}
+
+Student:~Student(){
+  delete &value;
+  nect = NULL
+}
+
+Student*Student::getNext(){
+  return student;
+}
